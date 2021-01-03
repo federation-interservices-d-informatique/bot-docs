a05e2db1a1a65cfd3e98628210b7c670875da513
Woomy <woomy@woomy.be>
2021-01-03 15:02:00 +0100

    ♻️ Migrate autoriseinvites to vue-discord-message

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,13 +3,27 @@ import VueDiscordMessage from 'vue-discord-message';
 export default ({ Vue }) => {
 	Vue.use(VueDiscordMessage, {
 		avatars: {
-			nio: require('../assets/nio.png'),
+            nio: require('../assets/nio.png'),
+            fii: require('../assets/fii.png'),
+            woomy: require('../assets/woomy.gif')
         },
         profiles: {
             nio: {
                 author: 'NiO_Obers',
-                roleColor: `#ff4093`
-            }
+                roleColor: `#ff4093`,
+                avatar: 'nio'
+            },
+            fiibot: {
+                author: `FII - Bot`,
+                bot: true,
+                roleColor: `#4dbcba`,
+                avatar: 'fii'
+            },
+            woomy: {
+                author: 'Woomy',
+                roleColor: `#ff4093`,
+                avatar: 'woomy'
+            },
         },
         'compact-mode': false
 	});
