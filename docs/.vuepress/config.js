3cf3b9b621e618211c77fd6d69a08f9e212e0bcb
Woomy <woomy@woomy.be>
2021-01-03 12:20:21 +0100

    🔧 Start configuring vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
   title: "FIIBot - Docs",
   description: "La documentation de FII - Bot",
+  base: "/bot-docs/",
+  theme: "yuu",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      {
+        text: "GitHub",
+        link: "https://github.com/Federation-Interservices-D-Informatique",
+      },
+    ],
+    sidebar: []
+  },
 };
